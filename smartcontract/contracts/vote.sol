65b257eb78d981cafa5b5e33deca650d8780a869
Samuel1505 <oluwakoredeakinola@gmail.com>
2025-11-05 00:20:05 +0100

    Refactored the voting function and added Contenders functions, getvote,getwinner, votingexpired etc functions

--- smartcontract/contracts/vote.sol
+++ smartcontract/contracts/vote.sol
@@ -1,19 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.14;
 
-contract VotingContract{
-    event contRegistered(
+contract VotingContract {
+    event ContenderRegistered(
         address indexed contender,
-        string indexed code
+        string code
     );
 
-    event voteSuccess(
+    event VoteSuccess(
         address indexed voter, 
         address indexed contender, 
-        string indexed code
+        string code
     ); 
 
-        event VotingEnded(
+    event VotingEnded(
         address[] winners,
         uint32 highestVotes
     );
@@ -25,12 +25,12 @@ contract VotingContract{
     bool public votingActive;
     bool public votingEnded;
 
-
     struct ContDetails {
         address contender;
         string code;
         uint32 votersNo;
-    }  
+        bool exists;
+    }
 
     address[] public contendersList;
     mapping(address => bool) public voted;
@@ -59,7 +59,8 @@ contract VotingContract{
         registrar = msg.sender;
     }
 
-        function registerContender(address cont, string memory code) public onlyRegistrar votingNotStarted {
+    // Register a single contender
+    function registerContender(address cont, string memory code) public onlyRegistrar votingNotStarted {
         require(cont != address(0), "Invalid contender address");
         require(bytes(code).length > 0, "Code cannot be empty");
         require(codeToAddress[code] == address(0), "Code already exists");
@@ -90,7 +91,8 @@ contract VotingContract{
         }
     }
 
-     function startVoting() public onlyRegistrar votingNotStarted {
+    // Start the voting process
+    function startVoting() public onlyRegistrar votingNotStarted {
         require(contendersList.length > 0, "No contenders registered");
         
         votingActive = true;
@@ -116,15 +118,115 @@ contract VotingContract{
         emit VoteSuccess(sender, contender, code);
     }
 
-    function getContender (string memory code) public view returns(ContDetails memory)  {
-        address c = codetoadd[code];
-        ContDetails storage m = contenderdet[c];
+    // End voting and determine winners
+    function endVoting() public onlyRegistrar {
+        require(votingActive, "Voting is not active");
+        require(block.timestamp > votingEndTime || votingEnded, "Voting period not ended");
+        
+        votingActive = false;
+        votingEnded = true;
 
-        return (m);
+        // Find highest vote count
+        uint32 highestVotes = 0;
+        for (uint256 i = 0; i < contendersList.length; i++) {
+            uint32 votes = contenderDetails[contendersList[i]].votersNo;
+            if (votes > highestVotes) {
+                highestVotes = votes;
+            }
+        }
+
+        // Collect all winners (in case of tie)
+        address[] memory winners = new address[](contendersList.length);
+        uint256 winnerCount = 0;
+        
+        for (uint256 i = 0; i < contendersList.length; i++) {
+            if (contenderDetails[contendersList[i]].votersNo == highestVotes) {
+                winners[winnerCount] = contendersList[i];
+                winnerCount++;
+            }
+        }
+
+        // Resize winners array
+        address[] memory finalWinners = new address[](winnerCount);
+        for (uint256 i = 0; i < winnerCount; i++) {
+            finalWinners[i] = winners[i];
+        }
+
+        emit VotingEnded(finalWinners, highestVotes);
     }
 
-    //register all 3 conteners at once(make sure that the registrar has power over the election they created)
-    //time frame for the voting process(given that the voting starts immediately after contenders registration) and 
-    //Counting of vote for each contenders
-    //automation to stop the voting when it time and paste winners accordinng to number of votes they got.
+    // Get contender details by code
+    function getContender(string memory code) public view returns(ContDetails memory) {
+        address cont = codeToAddress[code];
+        require(cont != address(0), "Contender not found");
+        return contenderDetails[cont];
+    }
+
+    // Get contender details by address
+    function getContenderByAddress(address cont) public view returns(ContDetails memory) {
+        require(contenderDetails[cont].exists, "Contender not found");
+        return contenderDetails[cont];
+    }
+
+    // Get all contenders
+    function getAllContenders() public view returns(address[] memory) {
+        return contendersList;
+    }
+
+    // Get vote counts for all contenders
+    function getVoteCounts() public view returns(address[] memory, uint32[] memory) {
+        uint256 length = contendersList.length;
+        address[] memory addresses = new address[](length);
+        uint32[] memory votes = new uint32[](length);
+
+        for (uint256 i = 0; i < length; i++) {
+            addresses[i] = contendersList[i];
+            votes[i] = contenderDetails[contendersList[i]].votersNo;
+        }
+
+        return (addresses, votes);
+    }
+
+    // Get winners (can be called after voting ends)
+    function getWinners() public view returns(address[] memory, uint32) {
+        require(votingEnded, "Voting has not ended");
+
+        uint32 highestVotes = 0;
+        for (uint256 i = 0; i < contendersList.length; i++) {
+            uint32 votes = contenderDetails[contendersList[i]].votersNo;
+            if (votes > highestVotes) {
+                highestVotes = votes;
+            }
+        }
+
+        address[] memory winners = new address[](contendersList.length);
+        uint256 winnerCount = 0;
+        
+        for (uint256 i = 0; i < contendersList.length; i++) {
+            if (contenderDetails[contendersList[i]].votersNo == highestVotes) {
+                winners[winnerCount] = contendersList[i];
+                winnerCount++;
+            }
+        }
+
+        address[] memory finalWinners = new address[](winnerCount);
+        for (uint256 i = 0; i < winnerCount; i++) {
+            finalWinners[i] = winners[i];
+        }
+
+        return (finalWinners, highestVotes);
+    }
+
+    // Check if voting time has expired
+    function hasVotingExpired() public view returns(bool) {
+        return votingActive && block.timestamp > votingEndTime;
+    }
+
+    // Get time remaining for voting
+    function getTimeRemaining() public view returns(uint256) {
+        if (!votingActive || block.timestamp >= votingEndTime) {
+            return 0;
+        }
+        return votingEndTime - block.timestamp;
+    }
 }
